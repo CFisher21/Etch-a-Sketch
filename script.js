ef96be94737b339741078017b0f7bd3b71ac25dd
Cody Fisher <fishercody23@gmail.com>
2024-04-09 21:28:20 -0600

    Created the pop up window to allow the user to prompt how big they wish the grid to be. Logic still needs to be added to take this input and make it adjust the grid size. Also commiting logic that allows the input setting-slider to update a number for feedback on what the user has selected from 1-10.

--- script.js
+++ script.js
@@ -1,29 +1,53 @@
-const grid = document.getElementById("grid");
-const gridTile = document.getElementById("grid-tile");
-const refreshButton = document.getElementById("refresh-button")
+function createGrid(gridsize) {
+    var gridSize = gridsize * gridsize
 
-function createGrid() {
-    //var gridsize = gridsize
-
-
-    for(let i = 0; i < 256; i++) {
+    for(let i = 0; i < gridSize; i++) {
         const newDiv = document.createElement("div");
         newDiv.classList.add("grid-tile")
         grid.appendChild(newDiv)
     }
 }
 
-createGrid();
+createGrid(16);
 
-grid.addEventListener("mouseover", (event) => {
-    if (event.target.classList.contains("grid-tile")) {
-        event.target.style.backgroundColor = "black";
-    }
-});
+document.addEventListener("DOMContentLoaded", function() {
+    const grid = document.getElementById("grid");
+    const refreshButton = document.getElementById("refresh-button");
+    const sizeButton = document.getElementById("size-button");
+    var slider = document.getElementById("setting-slider");
+    var sliderValue = document.getElementById("sliderValue");
+    const popUp = document.getElementById("pop-up-window");
+    let confirmBtn = document.getElementById("confirm-settings");
 
-refreshButton.addEventListener("click", () => {
-    location.reload();
+    grid.addEventListener("mouseover", (event) => {
+        if (event.target.classList.contains("grid-tile")) {
+            event.target.style.backgroundColor = "black";
+        }
+    });
+
+    refreshButton.addEventListener("click", () => {
+        location.reload();
+    })
+
+    sizeButton.addEventListener("click", () => {
+        popUp.style.display = "flex"
+    })
+
+    confirmBtn.addEventListener("click", () => {
+        popUp.style.display = "none"
+    })
+
+
+
+    // Adjust the value of the pop up slider
+    slider.addEventListener("input", function() {
+        sliderValue.textContent = slider.value;
+    });
+    
 })
+
+
+
 
 
 
