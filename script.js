c031443e58d1f52a609830965a44cf36ef3ec1cb
Cody Fisher <fishercody23@gmail.com>
2024-04-09 20:16:35 -0600

    Fixed the grid so that the color stays after mouse over. Added a refresh button so the application can be easily refreshed.

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
-const grid = document.getElementById("grid")
+const grid = document.getElementById("grid");
+const gridTile = document.getElementById("grid-tile");
+const refreshButton = document.getElementById("refresh-button")
 
 function createGrid() {
-   
+    //var gridsize = gridsize
+
+
     for(let i = 0; i < 256; i++) {
         const newDiv = document.createElement("div");
         newDiv.classList.add("grid-tile")
@@ -10,3 +14,16 @@ function createGrid() {
 }
 
 createGrid();
+
+grid.addEventListener("mouseover", (event) => {
+    if (event.target.classList.contains("grid-tile")) {
+        event.target.style.backgroundColor = "black";
+    }
+});
+
+refreshButton.addEventListener("click", () => {
+    location.reload();
+})
+
+
+
